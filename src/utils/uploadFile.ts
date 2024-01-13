fe67146c2eb9cd2d0fc5ac51fd2e881105294a17
Ngima29 <ngima.sherpa8681@gmail.com>
2024-01-13 12:11:39 +0545

    update uploadFile

--- src/utils/uploadFile.ts
+++ src/utils/uploadFile.ts
@@ -1,25 +1,25 @@
-import multer from 'multer';
-import * as path from 'path';
-import fs from 'fs';
-import {Request} from 'express'
-import { any } from 'joi';
+// import multer from 'multer';
+// import * as path from 'path';
+// import fs from 'fs';
+// import {Request} from 'express'
+// import { any } from 'joi';
 
-const storage = multer.diskStorage({
-    destination: (req:Request, file:any, callback:any) => {
-        const employeeImagesPath = path.resolve(__dirname, '../public/images');
-        fs.mkdir(employeeImagesPath, { recursive: true }, (err:any) => {
-            if (err) {
-                console.error('Error creating directory:', err);
-                callback(err, '');
-            } else {
-                console.log('Directory created:', employeeImagesPath);
-                callback(null, employeeImagesPath);
-            }
-        });
-    },
-    filename: (req, file, callback) => {
-        callback(null, file.originalname);
-    },
-});
+// const storage = multer.diskStorage({
+//     destination: (req:Request, file:any, callback:any) => {
+//         const employeeImagesPath = path.resolve(__dirname, '../public/images');
+//         fs.mkdir(employeeImagesPath, { recursive: true }, (err:any) => {
+//             if (err) {
+//                 console.error('Error creating directory:', err);
+//                 callback(err, '');
+//             } else {
+//                 console.log('Directory created:', employeeImagesPath);
+//                 callback(null, employeeImagesPath);
+//             }
+//         });
+//     },
+//     filename: (req, file, callback) => {
+//         callback(null, file.originalname);
+//     },
+// });
 
-export const upload = multer({ storage: storage });
+// export const upload = multer({ storage: storage });
--- src/validators/articleValidator.ts
+++ src/validators/articleValidator.ts
@@ -5,7 +5,7 @@ import { InputeArticleInterface } from '../interfaces';
 const createArticle = Joi.object<InputeArticleInterface>({
   title: stringSchema.required().label('Title'),
   author: stringSchema.required().label('Author'),
-  image: Joi.binary().optional().label('Image'),
+ // image: Joi.binary().optional().label('Image'),
   content: stringSchema.required().label('Content'),
   publishDate: dateSchema.required().label('Publish Date'),
   tags: arraySchema.items(stringSchema).required().label('Tags'),
@@ -14,7 +14,7 @@ const createArticle = Joi.object<InputeArticleInterface>({
 const updateArticle = Joi.object({
   title: stringSchema.optional().label('Title'),
   author: stringSchema.optional().label('Author'),
-  image: Joi.binary().optional().label('Image'),
+  //image: Joi.binary().optional().label('Image'),
   content: stringSchema.optional().label('Content'),
   publishDate: dateSchema.optional().label('Publish Date'),
   tags: arraySchema.items(stringSchema).optional().label('Tags'),
